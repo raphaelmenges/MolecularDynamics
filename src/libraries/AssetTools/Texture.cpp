b141221f20ad345d46f3d6b8299373a52076fec1
Nils Lichtenberg <nlichtenberg@uni-koblenz.de>
2016-01-19 15:35:19 +0100

    minor

--- src/libraries/AssetTools/Texture.cpp
+++ src/libraries/AssetTools/Texture.cpp
@@ -120,7 +120,7 @@ GLuint Texture::genUimageBuffer(int size)
     glBindTexture(GL_TEXTURE_1D, t);
     glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_1D, 0, GL_RGBA16F, w, h, 0, GL_RGBA, GL_FLOAT, pixels);
+    glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA16F, size, 0, GL_RGBA, GL_FLOAT, pixels);
     glBindImageTexture(0, t, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA16F);
 
     textureHandle = t;
