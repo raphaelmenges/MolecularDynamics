4a92b04af0971fb59212f0fa9b637db1bedf0904
raphaelmenges <unrealer2@yahoo.de>
2016-04-10 23:06:38 +0200

    Testing protein loading.

--- src/executables/PerfectSurfaceDetection/PerfectSurfaceDetection.cpp
+++ src/executables/PerfectSurfaceDetection/PerfectSurfaceDetection.cpp
@@ -1,11 +1,29 @@
 #include "PerfectSurfaceDetection.h"
 
+#include "Molecule/MDtrajLoader/MdTraj/MdTrajWrapper.h"
+#include "Molecule/MDtrajLoader/Data/Protein.h"
+
 #include <iostream>
 
 // ### Class implementation ###
 
 PerfectSurfaceDetection::PerfectSurfaceDetection()
 {
+    // ### Load molecule ###
+
+    // Path to protein molecule
+    std::vector<std::string> paths;
+    paths.push_back(std::string(RESOURCES_PATH) + "/molecules/PDB/1crn.pdb");
+
+    // Load protein
+    MdTrajWrapper mdwrap;
+    mupProtein = mdwrap.load(paths);
+
+    // Test protein
+    mupProtein->minMax();
+    glm::vec3 min = mupProtein->getMin();
+    std::cout << min.x << ", " << min.y << ", " << min.z << std::endl;
+
     // Create structure to find neighbors fast
 
     // Calculate Solvent Accesible Surface
--- src/executables/PerfectSurfaceDetection/PerfectSurfaceDetection.h
+++ src/executables/PerfectSurfaceDetection/PerfectSurfaceDetection.h
@@ -1,6 +1,11 @@
 #ifndef PERFECT_SURFACE_DETECTION_H_
 #define PERFECT_SURFACE_DETECTION_H_
 
+#include <memory>
+
+// Forward declaration instead of including (saved compile time)
+class Protein;
+
 class PerfectSurfaceDetection
 {
 public:
@@ -9,6 +14,9 @@ public:
     PerfectSurfaceDetection();
 
 private:
+
+    // Members
+    std::unique_ptr<Protein> mupProtein;
 };
 
 #endif // PERFECT_SURFACE_DETECTION_H_
