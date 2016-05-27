0f6dfe2b5fc69dee34c91a2167e10565ade6907c
raphaelmenges <unrealer2@yahoo.de>
2016-05-27 13:39:49 +0200

    Added imgui library.

--- src/cmake/DefaultExecutable.cmake
+++ src/cmake/DefaultExecutable.cmake
@@ -7,14 +7,25 @@ get_filename_component(ProjectId ${CMAKE_CURRENT_SOURCE_DIR} NAME)
 string(REPLACE " " "_" ProjectId ${ProjectId})
 project(${ProjectId})
 
+# ImGui
+message(STATUS ImGui)
+set(IM_GUI_DIR ${EXTERNALS_PATH}/imgui)
+file(GLOB IM_GUI
+    ${IM_GUI_DIR}/imgui.cpp
+    ${IM_GUI_DIR}/imgui_draw.cpp
+    ${IM_GUI_DIR}/examples/opengl3_example/imgui_impl_glfw_gl3.cpp)
+include_directories(${IM_GUI_DIR})
+
+# Other libraries
 include_directories(
     ${LIBRARIES_PATH}
 )
 
+# Collect own soruce code
 file(GLOB_RECURSE SOURCES *.cpp)
 file(GLOB_RECURSE HEADER *.h)
 
-add_executable(${ProjectId} ${SOURCES} ${HEADER})
+add_executable(${ProjectId} ${SOURCES} ${HEADER} ${IM_GUI})
 
 target_link_libraries(
     ${ProjectId}
